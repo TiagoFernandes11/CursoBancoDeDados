19d84d91ba35519475e403c95c00b7f93dc0babb
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-05-29 16:03:25 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -25,4 +25,84 @@ INSERT INTO CLIENTE(NOME,SEXO,ENDERECO,TELEFONE,CPF) VALUES('LILIAN','F','SENADO
 INSERT INTO CLIENTE VALUES('ANA','F','ANA@GLOBO.COM',85548962,'548556985','PRES ANTONIO CARLOS - CENTRO - SAO PAULO - SP'),
                           ('CARLA','F','CARLA@TERATI.COM.BR',7745828,'66587458','SAMUEL SILVA - CENTRO - BELO HORIZONTE - MG');
 
-select nome as cliente, sexo, email, endereco from cliente;
+/* ALIAS DE COLUNAS */
+select nome as cliente, sexo, email, endereco, now() as DATA_HORA from cliente;
+
+/* FILTRANDO */
+SELECT NOME, SEXO FROM CLIENTE
+WHERE SEXO = 'M';
+
+SELECT NOME, ENDERECO FROM CLIENTE
+WHERE SEXO = 'F';
+
+SELECT NOME, SEXO FROM CLIENTE
+WHERE ENDERECO = 'RJ';
+
+/* UTILIZANDO O LIKE */
+SELECT NOME, SEXO FROM CLIENTE
+WHERE ENDERECO LIKE 'RJ';
+
+/* CARACTER CORINGA % -> QUALQUER COISA */
+SELECT NOME, SEXO FROM CLIENTE
+WHERE ENDERECO LIKE '%RJ';
+
+SELECT NOME, SEXO, ENDERECO FROM CLIENTE
+WHERE ENDERECO LIKE '%RJ';
+
+SELECT NOME, SEXO, ENDERECO FROM CLIENTE
+WHERE ENDERECO LIKE 'OSCAR CURY%';
+
+SELECT NOME, SEXO, ENDERECO FROM CLIENTE
+WHERE ENDERECO LIKE '%CENTRO%';
+
+/* Resolvendo o exercício */
+
+CREATE DATABASE LIVRARIA;
+
+USE LIVRARIA;
+CREATE TABLE LIVROS(
+	LIVRO VARCHAR(100),
+	AUTOR VARCHAR(100),
+	SEXO CHAR(1),
+	PAGINAS INT(5),
+	EDITORA VARCHAR(30),
+	VALOR FLOAT(10,2),
+	UF CHAR(2),
+	ANO INT(4)
+);
+
+/* VERIFICANDO OS BANCOS */
+SHOW DATABASES;
+
+/* CONEVCTANDO A UM BANCO */
+USE LIVRARIA;
+
+
+INSERT INTO LIVROS VALUES('Cavaleiro Real','Ana Claudia','F',465,'Atlas',49.9,'RJ',2009);
+INSERT INTO LIVROS VALUES('SQL para leigos','João Nunes','M',450,'Addison',98,'SP',2018);
+INSERT INTO LIVROS VALUES('Receitas Caseiras','Celia Tavares','F',210,'Atlas',45,'RJ',2008);
+INSERT INTO LIVROS VALUES('Pessoas Efetivas','Eduardo Santos','M',390,'Beta',78,'RJ',2018);
+INSERT INTO LIVROS VALUES('Habitos Saudáveis','Eduardo Santos','M',630,'Beta',150,'RJ',2019);
+INSERT INTO LIVROS VALUES('A Casa Marrom','Hermes Macedo','M',250,'Bubba',60,'MG',2016);
+INSERT INTO LIVROS VALUES('Estacio Querido','Geraldo Francisco','M',310,'Insignia',100,'ES',2015);
+INSERT INTO LIVROS VALUES('Pra sempre amigas','Leda Silva','F',510,'Insignia',78,'ES',2011);
+INSERT INTO LIVROS VALUES('Copas Inesqueciveis','Marco Alcantara','M',200,'Larson',130,'RS',2018);
+INSERT INTO LIVROS VALUES('O poder da mente','Clara Mafra','F',120,'Continental',56,'SP',2017);
+
+/* Trazer todos os valores */
+select * from livros;
+
+/* Trazer todos os nomes dos livros e das editoras */
+select livro, editora from livros;
+
+/* Trazer todos os nomes dos livros e das UFS de autores masculinos */
+select livro, uf from livros where sexo = "M";
+
+/* Trazer todos os nomes dos livros numero de paginas pelo sexo feminino */
+select livro, paginas from livros where sexo = "F";
+
+/* Trazer todos os nomes dos livros e das editoras de são paulo*/
+select livro, editora from livros where uf = "SP";
+
+/* Dados dos autores do sexo masculino que tiveram publicados em sao paulo ou no rio de janeiro */
+select autor from livros where sexo = "M" and uf = "SP" or uf = "RJ";
